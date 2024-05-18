4420cc7d0fa3017079785e6ca508d93425a3a82a
morishitakouki <morimori.plane.0615@gmail.com>
2024-05-18 19:47:54 +0900

    merge

--- backend/app/controllers/api/v1/restaurants_controller.rb
+++ backend/app/controllers/api/v1/restaurants_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::RestaurantsController < ApplicationController
     def index
-<<<<<<< HEAD
         restaurants = Restaurant.all 
        
-=======
-        @restaurants = Restaurant.all 
-
-
->>>>>>> 95a2e1b8f8b3297f7db27e4d791585efa770faac
         render json: {
             restaurants: @restaurants 
         }, status: :ok 
